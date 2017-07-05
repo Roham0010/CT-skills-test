4c26cc8dc2cd210683b22f3c095e66080ee6a370
hassan.shojaei1 <hassan_shojaei38@yahoo.com>
2017-07-05 16:15:25 +0430

    create app layout view

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="fa" class="no-js">
+    <head>
+        <meta charset="UTF-8">
+        <title>CT-skills-test</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body id="app-layout">
+        @yield('content')
+    </body>
+</html>
