58b2d1e94dc31c74f39b9d873deb5bad2b95b21d
hassan.shojaei1 <hassan_shojaei38@yahoo.com>
2017-07-05 14:42:48 +0430

    pure laravel project as CT-skills-test created

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([[
+            'name' => str_random(10),
+            'email' => str_random(10) . '@gmail.com',
+            'password' => bcrypt('123456'),
+        ], [
+            'name' => 'test',
+            'email' => 'test@gmail.com',
+            'password' => bcrypt('123456'),
+            'created_at' => Carbon::now(),
+        ]]);
+    }
+}
